bab2c2cf5d9c454566ebe4680babed15e36f68a3
Tapian A P Tampubolon <tapibro@gmail.com>
2021-10-11 00:23:41 +0700

    Change lg to md responsiveness

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -44,7 +44,7 @@
                         <!-- Nested Row within Card Body -->
                         <div class="row">
                             <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
-                            <div class="col-lg">
+                            <div class="col-md">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Login</h1>
