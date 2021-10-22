267f439943776d27203466269047cddbdfd35f65
Tapibro <tapibro@gmail.com>
2021-10-22 14:17:04 +0700

    Add auth token API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +20,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/user/create_token', function (Request $request) {
+    if (isset($request->email) && isset($request->password) && isset($request->token_name)) {
+        $user = User::where('email', $request->email)->first();
+        
+        if (!$user || !Hash::check($request->password, $user->password))
+            return response('The provided credentials are incorrect.', 401);
+
+        return $user->createToken($request->token_name)->plainTextToken;
+    }
+
+    return response('No credentials provided.', 400);
 });
